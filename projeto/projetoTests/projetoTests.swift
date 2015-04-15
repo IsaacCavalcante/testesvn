99f3d3c147839afbbcc22d24ba598ecc0e12af59
IsaacCavalcante <isaaccavalcante@ymail.com>
2015-04-15 11:12:34 -0300

    Adicionei o projeto

---
+++ projeto/projetoTests/projetoTests.swift
@@ -0,0 +1,36 @@
+//
+//  projetoTests.swift
+//  projetoTests
+//
+//  Created by Isaac Cavalcante on 15/04/15.
+//  Copyright (c) 2015 Isaac Cavalcante. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class projetoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
